a265935fd8f7bdea0ae5c2e26c207bef126385ea
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2020-08-10 13:11:45 +0200

    add popup and notification, add useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
 import "./App.css";
 
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetter, setWrongLetters] = useState([]);
+
   return (
     <>
       <Header />
       <div className="game-container">
         <Figure />
         <WrongLetters />
-        <Word />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
